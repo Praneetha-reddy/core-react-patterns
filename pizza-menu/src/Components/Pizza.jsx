ce2da3ffde683029fa47ae693846025899471e98
Praneetha Reddy <praneethapuchalapalli20@gmail.com>
2026-02-24 19:44:41 +0530

    Initial commit of pizza menu and steps components

---
+++ pizza-menu/src/Components/Pizza.jsx
@@ -0,0 +1,14 @@
+function Pizza({ name, ingredients, price, photoName, soldOut }) {
+  return (
+    <li className={`pizza ${soldOut ? "sold-out " : ""}`}>
+      <img src={photoName} alt={name}></img>
+      <div>
+        <h3>{name}</h3>
+        <p>{ingredients}</p>
+        <span>{soldOut ? "SOLD OUT" : price}</span>
+      </div>
+    </li>
+  );
+}
+
+export default Pizza;
